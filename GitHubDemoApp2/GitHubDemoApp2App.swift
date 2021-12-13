8b4c01a6fd5ea04d1c565adbf92d8d6bfaf508bd
Maxmudov13 <maxmudovilyos2003@gmail.com>
2021-12-13 05:17:07 +0500

    Initial Commit

---
+++ GitHubDemoApp2/GitHubDemoApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp2App.swift
+//  GitHubDemoApp2
+//
+//  Created by user on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
